7e0cbdd26d00853e95a94d435352e88d89a3cdd9
z687wang <tonyzhe@foxmail.com>
2018-07-03 18:50:54 -0400

    :tada:

--- ImageProcess.py
+++ ImageProcess.py
@@ -85,7 +85,7 @@ labels = np.arange(len(all_labels))
 numOfEach = [len(list(group)) for key, group in groupby(y_train)]
 avgOfAll = np.mean(numOfEach)
 print("Average number of training images:", int(avgOfAll))
-ax.barh(labels, numOfEach, align='center', color='blue', orient='o')
+ax.barh(labels, numOfEach, align='center', color='blue')
 ax.set_yticks(labels)
 ax.set_yticklabels(all_labels)
 ax.invert_yaxis()  # labels read top-to-bottom
@@ -213,3 +213,187 @@ for i in range(0, n_classes):
     plt.axis('off')
     num_of_samples.append(len(x_selected))
 plt.show()
+
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
+
+def LeNet(x):
+    """
+    Implement classic lenet architecture in tensorflow
+    """
+    mu = 0
+    sigma = 0.1
+    
+    # Layer 1: Convolutional. Input = 32x32x3. Output = 28x28x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean = mu, stddev = sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    #  Activation.
+    conv1 = tf.nn.relu(conv1)
+
+    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    #  Layer 2: Convolutional. Output = 10x10x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    
+    #  Activation.
+    conv2 = tf.nn.relu(conv2)
+
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # SFlatten. Input = 5x5x16. Output = 400.
+    fc0   = flatten(conv2)
+    
+    # Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    
+    #  Activation.
+    fc1    = tf.nn.relu(fc1)
+    
+    dr1 = tf.nn.dropout(fc1, keep_prob) 
+    
+    # Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
+    fc2_b  = tf.Variable(tf.zeros(84))
+    fc2    = tf.matmul(dr1, fc2_W) + fc2_b
+    
+    # Activation.
+    fc2    = tf.nn.relu(fc2)
+    
+    
+    dr2 = tf.nn.dropout(fc2, keep_prob) 
+    
+    #  Layer 5: Fully Connected. Input = 84. Output = 10.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 43), mean = mu, stddev = sigma))
+    fc3_b  = tf.Variable(tf.zeros(43))
+    logits = tf.matmul(dr2, fc3_W) + fc3_b
+    
+    return logits
+
+x  =  tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+keep_prob = tf.placeholder(tf.float32)
+one_hot_y = tf.one_hot(y, 43)
+
+rate = 0.0001
+
+logits = LeNet(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+predict_operation = tf.argmax(logits, 1)
+predict_proba_operation = tf.nn.softmax(logits=logits)
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+def predict(X_data):
+    num_examples = len(X_data)
+    sess = tf.get_default_session()
+    predicted_proba = list()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x = X_data[offset:offset+BATCH_SIZE]
+        predicted_proba.extend( sess.run(predict_proba_operation, feed_dict={x: batch_x, keep_prob: 1.0}))
+    return predicted_proba
+
+
+from sklearn.utils import shuffle
+from time import time
+
+#Do some logging
+import logging, datetime
+logger = logging.getLogger()
+
+def setup_file_logger(log_file):
+    hdlr = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr) 
+    logger.setLevel(logging.INFO)
+
+def log(message):
+    #outputs to Jupyter console
+    print('{} {}'.format(datetime.datetime.now(), message))
+    #outputs to file
+    logger.info(message)
+
+setup_file_logger('training.log')
+EPOCHS = 150
+BATCH_SIZE = 128
+dropout = .3
+
+errors = list()
+
+saver = tf.train.Saver()
+start = time()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    log("Training... dropout = {} , batch_size = {} , learning rate = {}".format(dropout, BATCH_SIZE, rate))
+    print()
+    for i in range(EPOCHS):
+        
+        try:
+            X_train, y_train = shuffle(X_train, y_train)
+#             print("Before Train %d sec"%(time() - start))
+            
+            for offset in range(0, num_examples, BATCH_SIZE):
+                end = offset + BATCH_SIZE
+                batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+                sess.run(training_operation, feed_dict={x: batch_x, y: batch_y, keep_prob: 1 - dropout})
+            
+#             print("After Train %d sec"%(time() - start))
+            
+            validation_accuracy = evaluate(X_valid, y_valid)
+            training_accuracy = evaluate(X_train, y_train)
+            
+            errors.append((training_accuracy,validation_accuracy))
+            log("EPOCH %d - %d sec ..."%(i+1, time() - start))
+            log("Training error = {:.3f} Validation error = {:.3f}".format(1- training_accuracy , 
+                                                                                   1- validation_accuracy))
+            
+            print()
+            
+#             print("After error computation %d sec"%(time() - start))
+            if i > 5 and i % 3 == 0:
+                saver.save(sess, './models/lenet')
+                print("Model saved %d sec"%(time() - start))
+        except KeyboardInterrupt:
+            print('Accuracy Model On Test Images: {}'.format(evaluate(X_test,y_test)))   
+            break
+            
+    saver.save(sess, './models/lenet')
+plt.figure(figsize=(8,6))
+plt.title('Learning Curve')
+plt.plot([1 - el[0] for el in errors])
+plt.plot([1 - el[1] for el in errors])
+plt.ylim([-.01,0.35])
+plt.legend(['Training Error','Validation Error'])
+plt.tight_layout()
+plt.savefig('plots/learning_curve.png')
+plt.ylabel('Error')
+plt.xlabel('Epoch');
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('./models'))
+    print('Accuracy Model On Training Images: {:.2f}'.format(evaluate(X_train,y_train)))
+    print('Accuracy Model On Validation Images: {:.2f}'.format(evaluate(X_valid,y_valid))) 
+    print('Accuracy Model On Test Images: {:.2f}'.format(evaluate(X_test,y_test)))
